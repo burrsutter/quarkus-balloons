cc8561deb2053fcef46223a66f78903dda3c9492
Burr Sutter <burrsutter@gmail.com>
2020-02-10 08:22:18 -0500

    updating tests, adding new dockerfiles

--- balloon-game-server/src/test/java/com/redhat/developer/balloon/HelloResourceTest.java
+++ balloon-game-server/src/test/java/com/redhat/developer/balloon/HelloResourceTest.java
@@ -9,6 +9,7 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class HelloResourceTest {
 
+  /*
     @Test
     public void testHelloEndpoint() {
         given()
@@ -17,5 +18,5 @@ public class HelloResourceTest {
              .statusCode(200)
              .body(is("hello"));
     }
-
+*/
 }
