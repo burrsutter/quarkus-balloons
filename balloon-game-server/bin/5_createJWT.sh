23b12b80f6731c066e5cf3a914c4f643d3fcf515
Burr Sutter <burrsutter@gmail.com>
2020-02-15 12:29:14 -0500

    fixing up game play scripts

--- balloon-game-server/bin/5_createJWT.sh
+++ balloon-game-server/bin/5_createJWT.sh
@@ -5,3 +5,5 @@ set -eu
 set -o pipefail 
 
 mvn exec:java -Dexec.mainClass=com.redhat.developer.balloon.GenerateJwtToken -Dexec.classpathScope=test -Dexec.args="privkey 36000" 
+
+echo "Create a JWT_TOKEN env var output from the above command"
