c00319b3180eb3ca50ceab0de52973a2d3d0ea05
Burr Sutter <burrsutter@gmail.com>
2020-02-17 16:08:52 -0500

    azure deployment updates

--- balloon-game-mobile/src/app/+game/service/game.service.ts
+++ balloon-game-mobile/src/app/+game/service/game.service.ts
@@ -46,8 +46,9 @@ export class GameService {
     gold: 'golden'
   };
   configuration: Object = {};  
-  socketUrl: string = (environment.production) ? 'wss://gameserver-game.apps.aws.burrsutter.org/game' : 'ws://localhost:8080/game';
-  // socketUrl: string = (environment.production) ? '' : 'wss://gameserver-game.apps.gcp.burrsutter.dev/game';
+  // socketUrl: string = (environment.production) ? 'wss://gameserver-game.apps.aws.burrsutter.org/game' : 'ws://localhost:8080/game';
+  // socketUrl: string = (environment.production) ? 'wss://gameserver-game.apps.gcp.burrsutter.dev/game' : 'ws://localhost:8080/game';
+  socketUrl: string = (environment.production) ? 'wss://gameserver-game.apps.azr.burrsutter.net/game' : 'ws://localhost:8080/game';
   @Output() stateChange = new EventEmitter();
   @Output() configurationChange = new EventEmitter();
   @Output() achievementsChange = new EventEmitter();
