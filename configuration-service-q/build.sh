da5013356d18e0d811dde091c9a94a62baa37839
Ray Tsang <saturnism@gmail.com>
2020-02-18 11:53:32 -0500

    updated configuration service

---
+++ configuration-service-q/build.sh
@@ -0,0 +1,2 @@
+docker build -f src/main/docker/Dockerfile.openjdk -t gcr.io/devnexus-balloons/configuration-service-q .
+docker push gcr.io/devnexus-balloons/configuration-service-q
---
+++ configuration-service-sb/build.sh
@@ -0,0 +1,1 @@
+./mvnw compile jib:build
